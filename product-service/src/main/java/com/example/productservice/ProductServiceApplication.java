2c8747a00bcb9e2e5f019d631a286624eab0e866
DungNT2 <nguyentiendunga42002@gmail.com>
2024-03-20 21:29:53 +0700

    add async

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -3,6 +3,9 @@ package com.example.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,13 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Bean("asyncExecutor")
+	public TaskExecutor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(20);
+		executor.setMaxPoolSize(100);
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setThreadNamePrefix("AsyncExecutor-");
+		return executor;
+	}
 }
