9b4db96911c39b4f130f8bd75739019645041fa2
DungNT2 <nguyentiendunga42002@gmail.com>
2024-03-31 08:30:04 +0700

    feat(productService): add common service

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan({"com.example.commonservice"})
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
